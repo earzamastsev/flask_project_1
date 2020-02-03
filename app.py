4a2ed2f43111a68145e5113af9bef67262c06998
eugene <earzamastsev@gmail.com>
2020-02-03 10:49:59 +0500

    Update templates and data

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from data import *
+from collections import OrderedDict
+from random import sample
 
 app = Flask(__name__)
 app.debug = True
@@ -7,17 +10,26 @@ app.debug = True
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    tours_index = OrderedDict([(k, tours[k]) for k in sample(tours.keys(), 6)])
+    return render_template('index.html', departures=departures, tours=tours_index)
 
 
-@app.route('/departure/<departure>')
-def departure(departure):
-    return render_template('departure.html', departure=departure)
+@app.route('/departure/<from1>/')
+def departure(from1):
+    tours_filteres = {}
+    tours_list = []
+    for key, value in tours.items():
+        if value["departure"] == from1:
+            tours_filteres[key] = value
+            tours_list.append(value)
+    return render_template('departure.html', from1=from1, departures=departures, tours=tours_filteres,
+                           tours_list=tours_list)
 
 
 @app.route('/tour/<id>/')
 def tour(id):
-    return render_template('tour.html', id=id)
+    id = int(id)
+    return render_template('tour.html', tours=tours, departures=departures, id=id)
 
 
 app.run()
